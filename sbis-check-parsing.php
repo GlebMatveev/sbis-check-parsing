f5f3a30b52295f863f7e2b4e7bad77dfe15c328d
glebmatveev <glebmatveev@gmail.com>
2022-04-08 18:29:58 +0300

    Added comments

--- sbis-check-parsing.php
+++ sbis-check-parsing.php
@@ -1,28 +1,39 @@
 <?
-    $connection = Bitrix\Main\Application::getConnection();
-    $sqlHelper = $connection->getSqlHelper();
+	// db connection
+	$connection = Bitrix\Main\Application::getConnection();
+	$sqlHelper = $connection->getSqlHelper();
 
-    $sql = "SELECT `DATE_CREATE`, `LINK_PARAMS`, `CNT_FAIL_PRINT` FROM `b_sale_cashbox_check` WHERE `ORDER_ID` = '" . $order['ORDER']['ACCOUNT_NUMBER'] . "'";
-    
-    $recordset = $connection->query($sql);
-    while ($record = $recordset->fetch())
-    {
-        if ($record[CNT_FAIL_PRINT] != 1) {
+	// select record by order id
+	$sql = "SELECT `DATE_CREATE`, `LINK_PARAMS`, `CNT_FAIL_PRINT` FROM `b_sale_cashbox_check` WHERE `ORDER_ID` = '" . $order['ORDER']['ACCOUNT_NUMBER'] . "'";
+	
+	// execute query
+	$recordset = $connection->query($sql);
+	while ($record = $recordset->fetch())
+	{
+		// if there is no check printing error
+		if ($record[CNT_FAIL_PRINT] != 1) {
 
-            $arrCheck = explode( ';', $record[LINK_PARAMS] );
-        
-            $regNumberKkt = explode( '"', $arrCheck[1] );
-    
-            $dateArr = explode( '.', $record[DATE_CREATE] );
-        
-            $yearArr = explode( ' ', $dateArr[2] );
-            $yearShort = substr($yearArr[0], -2);
-        
-            $fiscalDocAttribute = explode( ':', $arrCheck[3] );
+			// parse check array
+			$arrCheck = explode( ';', $record[LINK_PARAMS] );
+		
+			// getting "reg_number_kkt"
+			$regNumberKkt = explode( '"', $arrCheck[1] );
+	
+			// parse date array
+			$dateArr = explode( '.', $record[DATE_CREATE] );
+		
+			// getting year
+			$yearArr = explode( ' ', $dateArr[2] );
+			$yearShort = substr($yearArr[0], -2);
+		
+			// getting "fiscal_doc_number"
+			$fiscalDocAttribute = explode( ':', $arrCheck[3] );
 
-            $link = "https://ofd.sbis.ru/rec/" . $regNumberKkt[1] . "/" . $dateArr[0] . $dateArr[1] . $yearShort . "/" . $fiscalDocAttribute[1];
-                    
-            echo "<a href=\"" . $link . "\" target=\"_blank\">Чек</a><br>";
-        }	
-    }
+			// build a link
+			$link = "https://ofd.sbis.ru/rec/" . $regNumberKkt[1] . "/" . $dateArr[0] . $dateArr[1] . $yearShort . "/" . $fiscalDocAttribute[1];
+					
+			// output a check link
+			echo "<a href=\"" . $link . "\" target=\"_blank\">Чек</a><br>";
+		}	
+	}
 ?>
